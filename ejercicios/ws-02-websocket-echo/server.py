22415d74a69e5f35b1690a78795e34212fc7387d
Milasch23 <kamyxsch@gmail.com>
2026-04-28 21:05:34 -0300

    Add: Ejercicios Sugeridos 1, 2 y 4 en 01-websocket-desde-cero.md y pdf con capturas de pantalla del funcionamiento

--- ejercicios/ws-02-websocket-echo/server.py
+++ ejercicios/ws-02-websocket-echo/server.py
@@ -1,6 +1,5 @@
 import asyncio
 from datetime import datetime
-
 from websockets.asyncio.server import serve
 
 
@@ -9,19 +8,45 @@ CLIENTES = set()
 
 async def handler(websocket):
     CLIENTES.add(websocket)
+    
+    username = None
+    
     try:
-        await websocket.send("Conectado al servidor WebSocket")
+        await websocket.send("Conectado al servidor WebSocket.")
+        
+        try:
+            first_message = await asyncio.wait_for(websocket.recv(), timeout=10)
+        except asyncio.TimeoutError:
+            await websocket.send("Error. Tiempo de espera agotado. Debes enviar un nombre.")
+            return
+        
+        clean_name = first_message.strip()
+        
+        if not clean_name or len(clean_name) > 30:
+            await websocket.send("Error: Nombre de usuario inválido (debe contener no más de 30 caracteres)")
+            return
+        
+        username = clean_name
+        
+        await websocket.send(f"Bienvenido {username}. Ahora puedes enviar mensajes")
+        
+        
         async for message in websocket:
             clean = message.strip()
+            
             if not clean:
-                await websocket.send("Error: mensaje vacio")
+                await websocket.send(f"[{username}] Error: mensaje vacío")
                 continue
+            
             if len(clean) > 200:
-                await websocket.send("Error: maximo 200 caracteres")
+                print(f"Mensaje largo detectado: {len(clean)} caracteres")
+                await websocket.send(f"[{username}] Error: máximo 200 caracteres")
                 continue
+                   
 
             timestamp = datetime.now().strftime("%H:%M:%S")
-            await websocket.send(f"[{timestamp}] eco: {clean}")
+            await websocket.send(f"[{timestamp}] {username} dice: {clean}")
+    
     finally:
         CLIENTES.remove(websocket)
 
